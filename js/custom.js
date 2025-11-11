2e7125560337de7546254860c8011287fec72d63
tascusfood-del <tascusfood@gmail.com>
2025-11-11 22:31:51 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------
-    File Name: custom.js
+    File Name: custom.js
 ---------------------------------------------------------------------*/
 
 $(function () {
@@ -66,23 +66,23 @@ $(function () {
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 	
 	$(document).ready(function() {
-	  var owl = $('.carousel-slider-post');
-	  owl.owlCarousel({
+	  var owl = $('.carousel-slider-post');
+	  owl.owlCarousel({
 		items: 1,
 		loop: true,
 		margin: 10,
 		autoplay: true,
 		autoplayTimeout: 3000,
 		autoplayHoverPause: true
-	  });	  
+	  });	  
 	});
 	
 	/* OwlCarousel - Banner Rotator Slider
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 	
 	$(document).ready(function() {
-	  var owl = $('.banner-rotator-slider');
-	  owl.owlCarousel({
+	  var owl = $('.banner-rotator-slider');
+	  owl.owlCarousel({
 		items: 1,
 		loop: true,
 		margin: 10,
@@ -92,60 +92,60 @@ $(function () {
 		autoplay: true,
 		autoplayTimeout: 3000,
 		autoplayHoverPause: true
-	  });	  
+	  });	  
 	});
 	
 	/* OwlCarousel - Product Slider
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 	
 	$(document).ready(function() {
-	  var owl = $('#product-in-slider');
-	  owl.owlCarousel({
+	  var owl = $('#product-in-slider');
+	  owl.owlCarousel({
 		loop: true,
 		nav: true,
 		margin: 10,
 		navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
 		responsive: {
-		  0: {
+		  0: {
 			items: 1
-		  },
-		  600: {
+		  },
+		  600: {
 			items: 2
-		  },
-		  960: {
+		  },
+		  960: {
 			items: 3
-		  },
-		  1200: {
+		  },
+		  1200: {
 			items: 4
-		  }
+		  }
 		}
-	  });
-	  owl.on('mousewheel', '.owl-stage', function(e) {
+	  });
+	  owl.on('mousewheel', '.owl-stage', function(e) {
 		if (e.deltaY > 0) {
-		  owl.trigger('next.owl');
+		  owl.trigger('next.owl');
 		} else {
-		  owl.trigger('prev.owl');
+		  owl.trigger('prev.owl');
 		}
 		e.preventDefault();
-	  });
+	  });
 	});
 	
 	/* Scroll to Top
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 	
 	$(window).on('scroll', function (){
-        scroll = $(window).scrollTop();
-        if (scroll >= 100){
-          $("#back-to-top").addClass('b-show_scrollBut')
-        }else{
-          $("#back-to-top").removeClass('b-show_scrollBut')
-        }
-      });
-      $("#back-to-top").on("click", function(){
-        $('body,html').animate({
-          scrollTop: 0
-        }, 1000);
-    });
+        scroll = $(window).scrollTop();
+        if (scroll >= 100){
+          $("#back-to-top").addClass('b-show_scrollBut')
+        }else{
+          $("#back-to-top").removeClass('b-show_scrollBut')
+        }
+      });
+      $("#back-to-top").on("click", function(){
+        $('body,html').animate({
+          scrollTop: 0
+        }, 1000);
+    });
 	
 	/* Contact-form
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
@@ -228,37 +228,37 @@ $(function () {
 		spaceBetween: 30,
 		breakpoints: {
 			1024: {
-			  slidesPerView: 3,
-			  spaceBetween: 30,
+			  slidesPerView: 3,
+			  spaceBetween: 30,
 			},
 			768: {
-			  slidesPerView: 2,
-			  spaceBetween: 30,
-			  slidesPerColumn: 1,
+			  slidesPerView: 2,
+			  spaceBetween: 30,
+			  slidesPerColumn: 1,
 			},
 			640: {
-			  slidesPerView: 2,
-			  spaceBetween: 20,
-			  slidesPerColumn: 1,
+			  slidesPerView: 2,
+			  spaceBetween: 20,
+			  slidesPerColumn: 1,
 			},
 			480: {
-			  slidesPerView: 1,
-			  spaceBetween: 10,
-			  slidesPerColumn: 1,
+			  slidesPerView: 1,
+			  spaceBetween: 10,
+			  slidesPerColumn: 1,
 			}
-		  },
+		  },
 		pagination: {
 			el: '.swiper-pagination',
 			clickable: true,
 			dynamicBullets: true
 		}
-    });
+    });
 
 	/* Countdown
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 	
 	$('[data-countdown]').each(function () {
-        var $this = $(this),
+        var $this = $(this),
 		finalDate = $(this).data('countdown');
 		$this.countdown(finalDate, function (event) {
 			var $this = $(this).html(event.strftime(''
@@ -268,76 +268,76 @@ $(function () {
 			+ '<div class="time-bar"><span class="time-box">%M</span> <span class="line-b">min</span></div> '
 			+ '<div class="time-bar"><span class="time-box">%S</span> <span class="line-b">sec</span></div>'));
 		});
-    });
+    });
 	
 	/* Deal Slider
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 	
 	$('.deal-slider').slick({
-        dots: false,
-        infinite: false,
+        dots: false,
+        infinite: false,
 		prevArrow: '.previous-deal',
 		nextArrow: '.next-deal',
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
 		infinite: false,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: false
-            }
-        }, {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }]
-    });
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
 	
 	/* News Slider
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 	
 	$('#news-slider').slick({
-        dots: false,
-        infinite: false,
+        dots: false,
+        infinite: false,
 		prevArrow: '.previous',
 		nextArrow: '.next',
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-            }
-        }, {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }]
-    });
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
 	
 	/* Fancybox
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
@@ -351,18 +351,20 @@ $(function () {
 	
 	/* Toggle sidebar
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     
-     $(document).ready(function () {
-       $('#sidebarCollapse').on('click', function () {
-          $('#sidebar').toggleClass('active');
-          $(this).toggleClass('active');
-       });
-     });
+     
+     $(document).ready(function () {
+       $('#sidebarCollapse').on('click', function () {
+          $('#sidebar').toggleClass('active');
+          $(this).toggleClass('active');
+       });
+     });
 
-     /* Product slider 
-     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     // optional
-     $('#blogCarousel').carousel({
-        interval: 5000
-     });
+     /* Product slider 
+     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+     // optional
+     $('#blogCarousel').carousel({
+        interval: 5000
+     });
 
+
+});
